5a3a82c211ffdd06fc040e04d7489f79e198dd58
Miroslav <mirino.bella@gmail.com>
2024-02-20 18:28:41 +0100

    initial commit

---
+++ src/test/java/com/example/RESTfullservice/ResTfullserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RESTfullservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfullserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
